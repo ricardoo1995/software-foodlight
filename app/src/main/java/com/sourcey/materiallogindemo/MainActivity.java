19094aa5851501744398e68332d605e5a7e8800b
Dei-Program <david.condor@epn.edu.ec>
2018-01-18 16:21:16 -0500

    comentarioddf hola

--- app/src/main/java/com/sourcey/materiallogindemo/MainActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+//holas
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
