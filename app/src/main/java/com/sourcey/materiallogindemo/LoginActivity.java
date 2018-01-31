7fbbbe7e9cdc55f03adebcbe139789c5c9be81ad
Dei-Program <david.condor@epn.edu.ec>
2018-01-31 11:37:22 -0500

    login facebook

--- app/src/main/java/com/sourcey/materiallogindemo/LoginActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/LoginActivity.java
@@ -15,7 +15,23 @@ import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.Bind;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
 
@@ -29,7 +45,37 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-        
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email","public_profile");
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                String userid = loginResult.getAccessToken().getUserId();
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                    displayUserInfo(object);
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "first_name, last_name, email, id");
+                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
+            }
+
+
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
         _loginButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -49,7 +95,34 @@ public class LoginActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });
+
+
     }
+    public void displayUserInfo(JSONObject object){
+        String first_name, last_name, email, id;
+        first_name="";
+        last_name="";
+        email="";
+        id="";
+        try {
+            first_name = object.getString("first_name");
+            last_name=object.getString("last_name");
+            email=object.getString("id");
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        TextView tv_name, tv_email,tv_id;
+        //tv_name=(TextView)findViewById(R.id.TV_name);
+        //tv_email=(TextView)findViewById(R.id.TV_email);
+        //tv_id=(TextView)findViewById(R.id.TV_id);
+
+        //tv_name.setText(first_name+""+last_name);
+        //tv_email.setText("Email : "+email);
+        //tv_id.setText("ID :"+id);
+
+
+    }
+
 
     public void login() {
         Log.d(TAG, "Login");
@@ -86,6 +159,9 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == REQUEST_SIGNUP) {
             if (resultCode == RESULT_OK) {
 
@@ -135,4 +211,5 @@ public class LoginActivity extends AppCompatActivity {
 
         return valid;
     }
+
 }
